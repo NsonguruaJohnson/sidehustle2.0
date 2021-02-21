0949b037bcc3644f4d8869b118fcb69af17b8196
Nsongurua Johnson <nsonguruajohnson@gmail.com>
2021-02-21 15:50:06 +0100

    Task 2

---
+++ Task2/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    // Airtime generator
+    // Assumming all networks have a 12 digits recharge pin
+    for($x = 0; $x <= 200; $x++){
+        echo $rand = rand(111111111111,999999999999);
+        echo '<br>';
+    }
+
+?>
